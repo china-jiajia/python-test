459c7cc113f5abb9a95b48faa625ac61ab93a7f0
Jiajia Tao <jiajia.tao@dena.com>
2018-05-11 13:36:42 +0800

    new file commit

--- Python2.7/mail.py
+++ Python2.7/mail.py
@@ -1,44 +1,29 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import  sys,os,re,urllib,urlparse
 import smtplib
-import traceback
 from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+import boto3
 
-def sendmail(subject,msg,toaddrs,fromaddr,smtpaddr,password):
-    '''
-    @subject:邮件主题
-    @msg:邮件内容
-    @toaddrs:收信人的邮箱地址
-    @fromaddr:发信人的邮箱地址
-    @smtpaddr:smtp服务地址，可以在邮箱看，比如163邮箱为smtp.163.com
-    @password:发信人的邮箱密码
-    '''
-    mail_msg = MIMEMultipart()
-    if not isinstance(subject,unicode):
-        subject = unicode(subject, 'utf-8')
-    mail_msg['Subject'] = subject
-    mail_msg['From'] =fromaddr
-    mail_msg['To'] = ','.join(toaddrs)
-    mail_msg.attach(MIMEText(msg, 'html', 'utf-8'))
-    try:
-        s = smtplib.SMTP()
-        s.connect(smtpaddr)  #连接smtp服务器
-        s.login(fromaddr,password)  #登录邮箱
-        s.sendmail(fromaddr, toaddrs, mail_msg.as_string()) #发送邮件
-        s.quit()
-    except Exception,e:
-       print "Error: unable to send email"
-       print traceback.format_exc()
+msg_from = 'jiajia664878380@163.com'
+passwd = 'JinG602162545'
+msg_to = '526453770@qq.com'
 
-if __name__ == '__main__':
-    fromaddr = "jiajia664878380@163.com"
-    smtpaddr = "smtp.163.com"
-    toaddrs = ["526453770@qq.com","jiajia664878380@163.com"]
-    subject = "测试邮件"
-    password = "JinG602162545"
-    msg = "测试一下"
-    sendmail(subject,msg,toaddrs,fromaddr,smtpaddr,password)
+subject = "亚马逊云实例状态管理"
+content = "Boto3 instance states"
+
+msg = MIMEText(content)
+msg['Subject'] = subject
+msg['From'] = msg_from
+msg['To'] = msg_to
+
+try:
+    s = smtplib.SMTP("smtp.163.com", 25)
+    s.login(msg_from, passwd)
+    s.sendmail(msg_from, msg_to, msg.as_string())
+    print ("发送成功")
+except smtplib.SMTPException as e:
+    print ("发送失败")
+finally:
+    s.quit()
 
